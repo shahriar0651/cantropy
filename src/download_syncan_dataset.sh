80f8f37ebef78bf0cf27ace9d5ff7b1cba8debba
Md Hasan Shahriar <shahriar.mdh@gmail.com>
2024-03-20 22:21:30 -0400

    Add License

--- src/download_syncan_dataset.sh
+++ src/download_syncan_dataset.sh
@@ -1,3 +1,30 @@
+# MIT License
+#
+# Copyright (c) 2023 Md Hasan Shahriar
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Project: CANtropy - Time Series Feature Extraction-Based Intrusion Detection Systems for Controller Area Networks
+# Author: Md Hasan Shahriar
+# Email: hshahriar@vt.edu
+#
+
 # Downloading syncan dataset from the github repo
 git clone https://github.com/etas/SynCAN.git ../../datasets/can-ids/syncan/
 echo "Raw SynCAN dataset downloaded in ../../datasets/can-ids/syncan/"
